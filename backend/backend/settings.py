edbf854daed22481913bf82873d344ec51776d35
Swasthik-shazab <swasthik@shazab.in>
2025-03-19 18:53:01 +0530

    restructured backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -133,8 +133,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'static',
 ]
 
 MEDIA_ROOT ='static/images'
