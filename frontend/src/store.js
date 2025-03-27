b3b6ba2a8f99d7af45b21f5331b5ff14ba5ac2da
SwasthikPB <swasthikpb@gmail.com>
2025-03-27 11:56:09 +0530

    added cart page

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,11 +2,33 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { productListReducer } from "./reducers/productReducer";
 import { productDetailsReducer } from "./reducers/productDetailsReducer";
+import { cartReducer } from "./reducers/cartReducer";
+
+const cartItemFromStorage = (() => {
+  try {
+    console.log(
+      "Cart Items in Local Storage:",
+      localStorage.getItem("cartItems")
+    );
+
+    return localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems"))
+      : [];
+  } catch (error) {
+    console.error("Error parsing cartItems from localStorage:", error);
+    return [];
+  }
+})();
+
+const initialState = {
+  cart: { cartItems: cartItemFromStorage },
+};
 
 const store = configureStore({
   reducer: {
     productList: productListReducer,
     productDetails: productDetailsReducer,
+    cart: cartReducer,
   },
 
   devTools: process.env.NODE_ENV !== "production",
